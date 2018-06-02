08000b79f4ef842fe5a9ec805be74725e7a4c1df
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-06-02 12:52:50 +0200

    :sparkles: Add filter dropdowns

---
+++ src/app/components/FilterRow/TournamentDropdown/TournamentDropdown.ts
@@ -0,0 +1,33 @@
+import { TennisActions } from './../../../store/tennis/index';
+import { Getter, Action } from 'vuex-class';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import { tournamentLevel } from '@/app/constants/tournament-levels';
+import { TennisGetters, TennisState } from '@/app/store/tennis';
+import { Player } from '@/app/models/Player';
+import TournamentIcon from './TournamentIcon/TournamentIcon';
+
+interface FilterOptions {
+  tournament: string;
+}
+
+@Component({
+  components: {
+    TournamentIcon,
+  },
+})
+export default class TournamentDropdown extends Vue {
+
+  @Getter(TennisGetters.Filters)
+  public filters: FilterOptions;
+
+  @Action(TennisActions.SetFilterTournament)
+  public setFilterTournament: (value: string) => void;
+
+  public tournaments: string[] = Object.values(tournamentLevel);
+
+  public onSelection(value: string): void {
+    this.setFilterTournament(value);
+  }
+
+}
---
+++ src/app/components/FilterRow/TournamentDropdown/TournamentDropdown.vue
@@ -0,0 +1,5 @@
+<template src="./TournamentDropdown.html"></template>
+<style lang="scss" scoped src="./TournamentDropdown.scss"></style>
+<script lang="ts" src="./TournamentDropdown.ts">
+</script>
+
