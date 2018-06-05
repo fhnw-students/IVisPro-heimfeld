77d489ea7424dff85645b135f76b823f25ed5ae3
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-06-05 12:52:37 +0200

    :lipstick: Some ui fixes

--- src/app/components/Head/Head.ts
+++ src/app/components/Head/Head.ts
@@ -1,10 +1,4 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import Spinner from '@/app/components/Spinner/Spinner.vue';
-
-@Component({
-  components: {
-    Spinner,
-  },
-})
+@Component
 export default class Head extends Vue { }
