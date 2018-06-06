c5a748c0e4d16f424fd0f634d1eabdf7100a5cd6
David Heimgartner <david.heimgartner@students.fhnw.ch>
2018-06-06 14:33:53 +0200

    :checkered_flag: add contibuters

--- src/app/services/api/github.api.ts
+++ src/app/services/api/github.api.ts
@@ -15,9 +15,9 @@ export const baseConfig = {
  * @returns List of contributors.
  */
 export async function getContributors(): Promise<Contributor[]> {
-  return [];
-  // const response = await Vue.$http.get('/repos/fhnw-students/IVisPro-heimfeld/contributors', Object.assign(baseConfig, {}));
-  // return plainToClass<Contributor, Contributor[]>(Contributor, response.data);
+  //return [];
+  const response = await Vue.$http.get('/repos/fhnw-students/IVisPro-heimfeld/contributors', Object.assign(baseConfig, {}));
+  return plainToClass<Contributor, Contributor[]>(Contributor, response.data);
 }
 
 /**
